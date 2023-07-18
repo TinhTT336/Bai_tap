4c4dbda0c26512d15641e2d2af51fb1af20dcfd0
TinhTT336 <138435713+TinhTT336@users.noreply.github.com>
2023-07-18 21:33:07 +0700

    Add files via upload

---
+++ btsession7/btap.js
@@ -0,0 +1,71 @@
+// const PI = 3.14;
+// let inputRadius = prompt("Enter the radius");
+// let radius = parseInt(inputRadius);
+// let diameterCircle = radius * 2 * PI;
+// let area = radius * radius * PI;
+// console.log(diameterCircle);
+// console.log(area);
+
+// let inputMath = prompt("Enter your Math grade");
+// let inputChem = prompt("Enter your Chemistry grade");
+// let inputBio = prompt("Enter your Biology grade");
+// let math = parseInt(inputMath);
+// let chem = parseInt(inputChem);
+// let bio = parseInt(inputBio);
+// let total = math + chem + bio;
+// let average = total / 3;
+// console.log(`Điểm tổng 3 môn là: ${total}`);
+// console.log(`Điểm trung bình là: ${average}`);
+// document.write("Điểm tổng 3 môn là:" + total);
+// document.write("Điểm trung bình là:" + average);
+
+// let inputCelsius = prompt("Độ C");
+// let censius = parseInt(inputCelsius);
+// let fah = censius / 5 * 9 + 32;
+// console.log(fah);
+// document.write("Độ F: " + fah);
+// alert("Độ F: " + fah);
+
+
+// let a = parseInt(prompt("Nhap a"));
+// let b = parseInt(prompt("Nhap b"));
+// let c = parseInt(prompt("Nhap c"));
+// let max = a;
+// if (max < b) {
+//     max = b;
+// }
+// if (max < c) {
+//     max = c;
+// }
+// document.write("So lon nhat trong 3 so la: " + max);
+
+
+// let n = parseInt(prompt("Nhap so luong gia tri trong day so"));
+// let a1 = parseInt(prompt("Nhap a1"));
+// let a2 = parseInt(prompt("Nhap a2"));
+// let a3 = parseInt(prompt("Nhap a3"));
+// let an = parseInt(prompt("Nhap n"));
+// let max = a1;
+// if (max < a2) { max = a2 };
+// if (max < a3) { max = a3 };
+// if (max < an) { max = an };
+// document.write("So lon nhat trong day so la: " + max);
+
+// console.log("Welcome to Rikkei Academy");
+// let age = prompt("Moi ban nhap so tuoi cua ban");
+// document.write("Quyet tam hoc lap trinh Javascript");
+// alert("Chao mung ban den voi hoc vien Rikkei Academy");
+// let result = confirm("Ban da du 18 tuoi");
+
+const RATE = 23000;
+let dollar = parseInt(amount);
+let vnd = dollar * RATE;
+document.write("Result:" + vnd + "VND");
+onclick = 'write("Ket qua la: "+vnd+ "VND")';
+
+   
+// document.write("Tao Button va Text bang Script<BR>");
+// document.write("<BR>");
+// document.write("<input type=button name=welcome value = 'Welcome' ");
+// document.write("onclick = 'alert ('Welcome to JavaScript');' > ");
+// document.write("<input type = text name = msg value = 'Welcome to'>");
